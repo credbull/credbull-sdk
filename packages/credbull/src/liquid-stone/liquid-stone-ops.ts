fdf4fab28f1348591931ca7af307a8db17881120
Ian Lucas <ian.lucas@gmail.com>
2025-04-22 17:44:08 -0400

    chore: Update LiquidStone Ops to be a class

--- packages/credbull/src/liquid-stone/liquid-stone-ops.ts
+++ packages/credbull/src/liquid-stone/liquid-stone-ops.ts
@@ -27,91 +27,115 @@ const plumeLegacyMainnetOpsConfig: OpsConfig = {
 //   nestVault: '0x593cCcA4c4bf58b7526a4C164cEEf4003C6388db',
 // };
 
-const liquidStone: LiquidStone = new LiquidStone(new CredbullClient(plumeLegacyMainnetOpsConfig));
+export class LiquidStoneOps {
+  private _opsConfig: OpsConfig;
+  private _liquidStone: LiquidStone;
+  private _isVerbose: boolean;
 
-async function toAssets(shares: bigint, depositPeriod: bigint): Promise<bigint> {
-  try {
-    return await liquidStone.convertToAssetsAtCurrentPeriod(shares, depositPeriod);
-  } catch (error) {
-    console.error(`-- id[${depositPeriod}] Unable to convert to assets! Error: ${error}`);
-    return toBigInt(0);
-  }
-}
-
-async function logBalances(depositPeriod: bigint, skipZeroBalance = false) {
-  const nestShares: bigint = await liquidStone.balanceOf(plumeLegacyMainnetOpsConfig.nestVault, depositPeriod);
-  const totalShares: bigint = await liquidStone.totalSupplyById(depositPeriod);
-
-  if (skipZeroBalance && nestShares == toBigInt(0) && totalShares == toBigInt(0)) {
-    return;
+  constructor(opsConfig: OpsConfig, isVerbose: boolean = false) {
+    this._opsConfig = opsConfig;
+    this._liquidStone = new LiquidStone(new CredbullClient(this._opsConfig));
+    this._isVerbose = isVerbose;
   }
 
-  console.log(
-    `-- id[${depositPeriod}] nestVault: ${await toStrShares(nestShares)}. (${await toStrUSDC(await toAssets(nestShares, depositPeriod))}).`,
-  );
-  console.log(
-    `-- id[${depositPeriod}] TOTAL    : ${await toStrShares(totalShares)}. (${await toStrUSDC(await toAssets(totalShares, depositPeriod))}).`,
-  );
+  async toAssets(shares: bigint, depositPeriod: bigint): Promise<bigint> {
+    try {
+      return await this._liquidStone.convertToAssetsAtCurrentPeriod(shares, depositPeriod);
+    } catch (error) {
+      console.error(`-- id[${depositPeriod}] Unable to convert to assets! Error: ${error}`);
+      return toBigInt(0);
+    }
+  }
 
-  if (totalShares != nestShares) {
-    console.warn(
-      `-- id[${depositPeriod}] !! WARNING !! Total and nestAgg balances differ by ${totalShares - nestShares} shares.  Check for other depositors / holders !!`,
+  async logBalances(depositPeriod: bigint, skipZeroBalance = false) {
+    const nestShares: bigint = await this._liquidStone.balanceOf(plumeLegacyMainnetOpsConfig.nestVault, depositPeriod);
+    const totalShares: bigint = await this._liquidStone.totalSupplyById(depositPeriod);
+
+    if (skipZeroBalance && nestShares == toBigInt(0) && totalShares == toBigInt(0)) {
+      return;
+    }
+
+    console.log(
+      `-- id[${depositPeriod}] nestVault: ${await toStrShares(nestShares)}. (${await toStrUSDC(await this.toAssets(nestShares, depositPeriod))}).`,
     );
+    console.log(
+      `-- id[${depositPeriod}] TOTAL    : ${await toStrShares(totalShares)}. (${await toStrUSDC(await this.toAssets(totalShares, depositPeriod))}).`,
+    );
+
+    if (totalShares != nestShares) {
+      console.warn(
+        `-- id[${depositPeriod}] !! WARNING !! Total and nestAgg balances differ by ${totalShares - nestShares} shares.  Check for other depositors / holders !!`,
+      );
+    }
   }
-}
 
-async function logRequestRedeems() {
-  const nestRequestReedemsArray = await liquidStone.unlockRequestsAll(plumeLegacyMainnetOpsConfig.nestVault);
-  console.log(`-- nestVault redeem requests: ${JSON.stringify(nestRequestReedemsArray, stringifyWithBigint())}`);
-}
-
-async function logAmountToInvest(depositPeriod: bigint) {
-  const amountToInvest = await liquidStone.amountToInvest(plumeLegacyMainnetOpsConfig.nestVault, depositPeriod);
-  console.log(`-- id[${depositPeriod}] Amount to Invest(+)/Redeem(-): ${await toStrUSDC(amountToInvest)}`);
-}
-
-async function verboseLogging(startPeriod: bigint, endPeriod: bigint) {
-  console.log(`Logging non-zero share balances from id[${startPeriod}] -> id[${endPeriod}]`);
-  for (let depositPeriod = Number(startPeriod); depositPeriod <= Number(endPeriod); depositPeriod++) {
-    await logBalances(toBigInt(depositPeriod), true);
+  async logRequestRedeems() {
+    const nestRequestReedemsArray = await this._liquidStone.unlockRequestsAll(this._opsConfig.nestVault);
+    console.log(`-- nestVault redeem requests: ${JSON.stringify(nestRequestReedemsArray, stringifyWithBigint())}`);
   }
-  console.log();
-}
 
-async function logDepositPeriod(depositPeriod: bigint) {
-  const currentPeriod = Number(await liquidStone.currentPeriod());
+  async logAmountToInvest(depositPeriod: bigint) {
+    const amountToInvest = await this._liquidStone.amountToInvest(this._opsConfig.nestVault, depositPeriod);
+    console.log(`-- id[${depositPeriod}] Amount to Invest(+)/Redeem(-): ${await toStrUSDC(amountToInvest)}`);
+  }
 
-  console.log(`Operations for period id[${depositPeriod}] (Current Period - ${currentPeriod - Number(depositPeriod)})`);
+  async verboseLogging(startPeriod: bigint, endPeriod: bigint) {
+    console.log(`Logging non-zero share balances from id[${startPeriod}] -> id[${endPeriod}]`);
+    for (let depositPeriod = Number(startPeriod); depositPeriod <= Number(endPeriod); depositPeriod++) {
+      await this.logBalances(toBigInt(depositPeriod), true);
+    }
+    console.log();
+  }
 
-  await logBalances(depositPeriod);
-  await logRequestRedeems();
-  await logAmountToInvest(depositPeriod);
-  console.log();
+  async logDepositPeriod(depositPeriod: bigint) {
+    const currentPeriod = Number(await this._liquidStone.currentPeriod());
+
+    console.log(
+      `Operations for period id[${depositPeriod}] (Current Period - ${currentPeriod - Number(depositPeriod)})`,
+    );
+
+    await this.logBalances(depositPeriod);
+    await this.logRequestRedeems();
+    await this.logAmountToInvest(depositPeriod);
+    console.log();
+  }
+
+  async main(isVerbose = false) {
+    console.log('Starting LiquidStone Ops checks...');
+    console.log();
+
+    const currentPeriod: bigint = await this._liquidStone.currentPeriod();
+
+    const valuePadding = 25;
+
+    console.log(`Current Period: id[${currentPeriod}]`);
+    console.log(
+      `Total Shares      : ${(await toStrShares(await this._liquidStone.totalSupply())).padStart(valuePadding)}`,
+    );
+    console.log(
+      `Total Asset Value : ${(await toStrUSDC(await this._liquidStone.totalAssets())).padStart(valuePadding)}`,
+    );
+    console.log(
+      `Asset Balance     : ${(await toStrUSDC(await this._liquidStone.assetBalance())).padStart(valuePadding)}`,
+    );
+
+    console.log();
+
+    if (isVerbose) {
+      await this.verboseLogging(toBigInt(0), currentPeriod);
+    }
+
+    await this.logDepositPeriod(currentPeriod - toBigInt(1));
+    await this.logDepositPeriod(currentPeriod);
+
+    console.log('LiquidStone Ops checks completed!');
+  }
 }
 
 async function main(isVerbose = false) {
-  console.log('Starting LiquidStone Ops checks...');
-  console.log();
+  const liquidStoneOps = new LiquidStoneOps(plumeLegacyMainnetOpsConfig, isVerbose);
 
-  const currentPeriod: bigint = await liquidStone.currentPeriod();
-
-  const valuePadding = 25;
-
-  console.log(`Current Period: id[${currentPeriod}]`);
-  console.log(`Total Shares      : ${(await toStrShares(await liquidStone.totalSupply())).padStart(valuePadding)}`);
-  console.log(`Total Asset Value : ${(await toStrUSDC(await liquidStone.totalAssets())).padStart(valuePadding)}`);
-  console.log(`Asset Balance     : ${(await toStrUSDC(await liquidStone.assetBalance())).padStart(valuePadding)}`);
-
-  console.log();
-
-  if (isVerbose) {
-    await verboseLogging(toBigInt(0), currentPeriod);
-  }
-
-  await logDepositPeriod(currentPeriod - toBigInt(1));
-  await logDepositPeriod(currentPeriod);
-
-  console.log('LiquidStone Ops checks completed!');
+  await liquidStoneOps.main(isVerbose);
 }
 
 // Entry point: handle errors globally
